384b3b67c7dc720610df20e33f2d2806e8da7f2b
cjang5189@gmail.com <cjang5189@gmail.com>
2025-05-07 22:22:32 +0900

    App클래스 실행시 db 연결하도록 리팩토링

--- src/DB.sql
+++ src/DB.sql
@@ -1,21 +1,28 @@
-SHOW TABLES;
 
-DROP TABLES `article`;
+DROP DATABASE IF EXISTS `AM_JDBC_2025_05`;
+CREATE DATABASE `AM_JDBC_2025_05`;
+USE `AM_JDBC_2025_05`;
 
 CREATE TABLE `article`(
-`id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-`regDate` DATETIME NOT NULL,
-`updateDate` DATETIME NOT NULL,
-`title` CHAR(100) NOT NULL,
-`body` TEXT NOT NULL
-
+    `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `regDate` DATETIME NOT NULL,
+    `updateDate` DATETIME NOT NULL,
+    `title` CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
 );
 
 INSERT INTO `article`
 SET `regDate` = NOW(),
     `updateDate` = NOW(),
-    `title` = '제목1',
-    `body` = '내용1';
+    `title` = '제목2',
+    `body` = '내용2';
 
 SELECT *
-FROM `article`;
+FROM `article`
+ORDER BY `id` DESC;
+
+UPDATE `article`
+SET `updateDate` = NOW(),
+    `title` = '제목1',
+    `body` = '제목1'
+WHERE `id` = 1;
