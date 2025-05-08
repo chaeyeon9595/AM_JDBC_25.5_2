7dca3b0854e291d69582ec4122b21856f9d34e3b
cjang5189@gmail.com <cjang5189@gmail.com>
2025-05-08 20:36:24 +0900

    SecSql, DBUtil 적용해서 write, list 수정

--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -1,6 +1,8 @@
 
 package org.example;
 
+import java.util.Map;
+
 public class Article {
     private int id;
     private String regDate;
@@ -15,6 +17,15 @@ public class Article {
         this.title = title;
         this.body = body;
     }
+
+    public Article(Map<String, Object> articleMap) {
+        this.id = (int)articleMap.get("id");
+        this.regDate = (String)articleMap.get("regDate");
+        this.updateDate = (String)articleMap.get("updateDate");
+        this.title = (String)articleMap.get("title");
+        this.body = (String)articleMap.get("body");
+    }
+
 
     @Override
     public String toString() {
