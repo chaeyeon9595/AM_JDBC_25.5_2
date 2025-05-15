8e9278e771fce641a5b0f0c1eb708a98d2160bd9
cjang5189@gmail.com <cjang5189@gmail.com>
2025-05-15 21:28:05 +0900

    게시글 삭제기능 리팩토링

--- src/main/java/org/example/service/ArticleService.java
+++ src/main/java/org/example/service/ArticleService.java
@@ -1,6 +1,26 @@
+
 package org.example.service;
 
-public class ArticleService {
-}
+import org.example.dao.ArticleDao;
 
-//
+import java.sql.Connection;
+import java.util.Map;
+
+public class ArticleService {
+
+    private Connection conn = null;
+    private ArticleDao articleDao = null;
+
+    public ArticleService(Connection conn) {
+        this.conn = conn;
+        this.articleDao = new ArticleDao(conn);
+    }
+
+    public Map<String, Object> getArticleById(int id) {
+        return articleDao.getArticleById(id);
+    }
+
+    public void doDelete(int id) {
+        articleDao.doDelete(id);
+    }
+}
